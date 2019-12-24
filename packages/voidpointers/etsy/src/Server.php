226ed66b5de2990c29f561e4af701171512fdc05
bryan <wangyabener@qq.com>
2019-12-24 16:21:21 +0800

    Init commit

---
+++ packages/voidpointers/etsy/src/Server.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Voidpointers\Etsy;
+
+use Gentor\OAuth1Etsy\Client\Server\Etsy;
+use League\OAuth1\Client\Credentials\TokenCredentials;
+
+class Server
+{
+    protected $server;
+
+    public function __construct()
+    {
+        $this->session = app('session');
+
+        $config = config('etsy');
+        $this->server = new Etsy([
+            'identifier' => $config['consumer_key'],
+            'secret' => $config['consumer_secret'],
+            'scope' => $config['scope'],
+            'callback_uri' => ''
+        ]);
+
+        if (!empty($config['access_token']) && !empty($config['access_token_secret'])) {
+            $tokenCredentials = new TokenCredentials();
+            $tokenCredentials->setIdentifier($config['access_token']);
+            $tokenCredentials->setSecret($config['access_token_secret']);
+            $this->tokenCredentials = $tokenCredentials;
+        }
+    }
+
+    /**
+     * @param $callbackUri
+     * @return string
+     */
+    public function authorize($callbackUri)
+    {
+        $this->server->getClientCredentials()->setCallbackUri($callbackUri);
+        // Retrieve temporary credentials
+        $temporaryCredentials = $this->server->getTemporaryCredentials();
+        // Store credentials in the session, we'll need them later
+        $this->session->put('temporary_credentials', serialize($temporaryCredentials));
+        return $this->server->getAuthorizationUrl($temporaryCredentials);
+    }
+
+    /**
+     * @param $token
+     * @param $verifier
+     * @return \League\OAuth1\Client\Credentials\TokenCredentials
+     */
+    public function approve($token, $verifier)
+    {
+        // Retrieve the temporary credentials we saved before
+        $temporaryCredentials = unserialize($this->session->get('temporary_credentials'));
+        return $this->server->getTokenCredentials($temporaryCredentials, $token, $verifier);
+    }
+
+    /**
+     * Get the user's unique identifier (primary key).
+     *
+     * @param bool $force
+     *
+     * @return string|int
+     */
+    public function getUserUid($force = false)
+    {
+        $userDetails = $this->getUserDetails($force);
+        return $userDetails->uid;
+    }
+
+    /**
+     * Get user details by providing valid token credentials.
+     *
+     * @param bool $force
+     *
+     * @return \League\OAuth1\Client\Server\User
+     */
+    public function getUserDetails($force = false)
+    {
+        return $this->server->getUserDetails($this->tokenCredentials, $force);
+    }
+
+    /**
+     * @param $method
+     * @param array $args
+     * @return array
+     */
+    public function __call($method, array $args)
+    {
+        $api = new Request($this->server, $this->tokenCredentials);
+        return call_user_func_array([$api, $method], $args);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/etsy', function () use ($router) {
+    $args = array(
+        'params' => array(
+            'listing_id' => 654321
+        ),
+        // A list of associations
+        'associations' => array(
+            // Could be a simple association, sending something like: ?includes=Images
+            'Images',
+            // Or a composed one with (all are optional as Etsy API says) "scope", "limit", "offset", "select" and sub-associations ("associations")
+            // ?includes=ShippingInfo(currency_code, primary_cost):active:1:0/DestinationCountry(name,slug)
+            'ShippingInfo' => array(
+                'scope' => 'active',
+                'limit' => 1,
+                'offset' => 0,
+                'select' => array('currency_code', 'primary_cost'),
+                // The only issue here is that sub-associations couldn't be more than one, I guess.
+                'associations' => array(
+                    'DestinationCountry' => array(
+                        'select' => array('name', 'slug')
+                    )
+                )
+            )
+        )
+    );
+    $result = Etsy::getListing($args);
+    var_dump($result);
+});
+
