b9558366885b28458eda79289b80553b3a5cb0da
brayn <voidpointers@hotmail.com>
2020-03-03 19:26:14 +0800

    fix

--- packages/voidpointers/etsy/src/Server.php
+++ packages/voidpointers/etsy/src/Server.php
@@ -23,7 +23,7 @@ class Server
             'callback_uri' => ''
         ]);
 
-        if (empty($config['access_token']) || empty($config['access_token_secret'])) {
+        if (!empty($config['access_token']) && !empty($config['access_token_secret'])) {
             $tokenCredentials = new TokenCredentials();
             $tokenCredentials->setIdentifier($config['access_token']);
             $tokenCredentials->setSecret($config['access_token_secret']);
