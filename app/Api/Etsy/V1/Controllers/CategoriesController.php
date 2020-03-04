6cb1d90862e0706263f16becc74f5bfba1780d09
brayn <voidpointers@hotmail.com>
2020-03-04 11:28:38 +0800

    处理多级分类

--- app/Api/Etsy/V1/Controllers/CategoriesController.php
+++ app/Api/Etsy/V1/Controllers/CategoriesController.php
@@ -3,15 +3,29 @@
 namespace Api\Etsy\V1\Controllers;
 
 use Api\Controller;
+use Dingo\Api\Http\Request;
 
 class CategoriesController extends Controller
 {
     public function index()
     {
-        return [
-            'top' => \Etsy::findAllTopCategory(),
-            'sub' => \Etsy::findAllTopCategoryChildren(),
-            'third' => \Etsy::findAllSubCategoryChildren()
-        ];
+        return \Etsy::findAllTopCategory();
+    }
+
+    public function sub(Request $request)
+    {
+        return \Etsy::findAllTopCategoryChildren([
+            'params' => ['tag' => $request->input('tag')],
+        ]);
+    }
+
+    public function third(Request $request)
+    {
+        return \Etsy::findAllSubCategoryChildren([
+            'params' => [
+                'tag' => $request->input('tag'),
+                'subtag' => $request->input('subtag'),
+            ]
+        ]);
     }
 }
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -11,7 +11,9 @@ $api->version('v1', [], function ($api) {
         $api->get('shop/{shop_id}', 'ShopsController@index');
         $api->post('shop/{shop_id}', 'ShopsController@update');
         $api->post('trackings/create', 'TrackingsController@create');
-        $api->get('category', 'CategoriesController@index');
+        $api->get('category/top', 'CategoriesController@index');
+        $api->get('category/sub', 'CategoriesController@sub');
+        $api->get('category/3rd', 'CategoriesController@third');
     });
     $api->group([
         'namespace' => 'Api\Etsy\V1\Controllers',
